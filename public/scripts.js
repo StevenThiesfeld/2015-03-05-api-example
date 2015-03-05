05889b8d4b87c318d9e35ce706b0a2919da4a551
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-05 14:35:41 -0600

    working on functions for links

---
+++ public/scripts.js
@@ -0,0 +1,27 @@
+function showStudents(){
+  var request = new XMLHttpRequest;
+  request.open("get", "http://127.0.0.1:4567/students");
+  request.send();
+  request.addEventListener("load", displayStudents, false);
+}
+
+function foo(){
+  var testP = document.createElement("P");
+  var testTxt = document.createTextNode("This is a test");
+  testP.appendChild(testTxt);
+  document.getElementById("response").appendChild(testP)
+}
+
+var displayStudents = function(event){
+   objects = JSON.parse(event.target.response);
+  for (i = 0; i < objects.length; i ++){
+    var p = document.createElement("P");
+    var txt = document.createTextNode(JSON.stringify(objects[i]));
+    p.appendChild(txt);
+    document.getElementById("response").appendChild(p)
+  }
+}
+
+function formatObject(object){
+  var objectString = object.id + 
+}
